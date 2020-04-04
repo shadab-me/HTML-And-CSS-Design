512934227e24ce7a0d49fcd0d7a04bfa2bcaffa7
Shadab Ali <shadabali604@gmail.com>
2020-04-04 19:18:25 +0530

    method added array

--- codeacadmy/array-method/app.js
+++ codeacadmy/array-method/app.js
@@ -1,3 +1,13 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 
+ 
 secretMessage.pop();
+secretMessage.length
+secretMessage.push('to', 'Program');
+
+secretMessage[7] = easily;
+secretMessage.shift();
+secretMessage.slice(0 , 5) = 'know';
+console.log(arr.join(' '));
+
+
